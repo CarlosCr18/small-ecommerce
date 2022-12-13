ebe8d7f3086b2ac83595f972fa6986be4dbc6ebb
CarlosCr18 <carloslcrespo18@gmail.com>
2022-12-13 16:11:35 -0600

    updated backend url

--- src/storeReducers/apiSlice/apiSlice.js
+++ src/storeReducers/apiSlice/apiSlice.js
@@ -3,7 +3,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 export const api = createApi({
   reducerPath: "api",
   baseQuery: fetchBaseQuery({
-    baseUrl: "https://small-ecommerce-backend.herokuapp.com/",
+    baseUrl: "https://small-ecommerce-backend.onrender.com/",
   }),
   tagTypes: ["Orders", "Data"],
   endpoints: (builder) => ({
